5847cf2fd5dc3b8564613d1e0f62ab6145eb2e13
wajahatdeveloper <58264405+wajahatdeveloper@users.noreply.github.com>
2024-04-18 20:01:54 +0500

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,4 +255,11 @@
 
 		}
 
+		window.onload = function() {
+			var links = document.getElementsByTagName('a');
+			for (var i = 0; i < links.length; i++) {
+				links[i].setAttribute('target', '_blank');
+			}
+		};
+
 })(jQuery);
